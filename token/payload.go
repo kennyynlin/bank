16b2cf2209c3870cc1f8227bd0e9f439662bfe7e
kennyynlin <ynlin1996@icloud.com>
2024-04-28 19:37:33 -0700

    Add implementation and testing of JWT and PASETO

---
+++ token/payload.go
@@ -0,0 +1,41 @@
+package token
+
+import (
+	"errors"
+	"github.com/google/uuid"
+	"time"
+)
+
+var (
+	ErrorExpiredToken = errors.New("token expired")
+	ErrorInvalidToken = errors.New("invalid token")
+)
+
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+func (payload *Payload) Valid() error {
+	if time.Now().After(payload.ExpiredAt) {
+		return ErrorExpiredToken
+	}
+	return nil
+}
+
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+	return payload, nil
+}
