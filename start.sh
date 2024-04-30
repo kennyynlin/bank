540929c7289f5e275312631bb0b82cad6fd00df2
kennyynlin <ynlin1996@icloud.com>
2024-04-30 15:32:47 -0700

    Dev (#10)
    * Add checkout code step
    
    * Add loading aws secrets step
    
    * Add loading env before running db migration
    
    * Add echo
    
    * Add fix source and echo db source

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@
 set -e
 
 echo "run db migration"
-source /app/app.env
+. /app/app.env
+echo "$DB_SOURCE"
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
