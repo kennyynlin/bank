78edefe3d78e0d626fdc1dcfb5239c2a474c16c7
kennyynlin <ynlin1996@icloud.com>
2024-05-05 17:49:14 -0700

    Add gRPC params validation

---
+++ val/validator.go
@@ -0,0 +1,55 @@
+package val
+
+import (
+	"fmt"
+	"net/mail"
+	"regexp"
+)
+
+var (
+	isValidUsername = regexp.MustCompile("^[a-z0-9_]+$").MatchString
+	isValidFullName = regexp.MustCompile("^[a-zA-Z\\s]+$").MatchString
+)
+
+func ValidateString(value string, minLength int, maxLength int) error {
+	n := len(value)
+	if n < minLength || n > maxLength {
+		return fmt.Errorf("string length must be between %d and %d", minLength, maxLength)
+	}
+	return nil
+}
+
+func ValidateUsername(value string) error {
+	if err := ValidateString(value, 3, 100); err != nil {
+		return err
+	}
+	if !isValidUsername(value) {
+		return fmt.Errorf("username must only contain alphanumeric characters and underscore")
+	}
+	return nil
+}
+
+func ValidatePassword(value string) error {
+	return ValidateString(value, 6, 100)
+}
+
+func ValidateEmail(value string) error {
+	if err := ValidateString(value, 3, 200); err != nil {
+		return err
+	}
+
+	if _, err := mail.ParseAddress(value); err != nil {
+		return fmt.Errorf("not a valid email address")
+	}
+	return nil
+}
+
+func ValidateFullName(value string) error {
+	if err := ValidateString(value, 3, 100); err != nil {
+		return err
+	}
+	if !isValidFullName(value) {
+		return fmt.Errorf("full name must only contain letters and spaces")
+	}
+	return nil
+}
